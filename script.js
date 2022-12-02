5a00899885a5119d356c31bf7865fa9a7413ada6
paulo-offi <65207464+paulo-offi@users.noreply.github.com>
2022-12-02 11:52:39 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+function scroll_home() {
+    window.scrollTo(0, 0);
+}
+
+function scroll_sobre_mim() {
+    window.scrollTo(0, 480)
+}
+
+function scroll_projetos() {
+    window.scrollTo(0, 950)
+}
+
+function scroll_habilidade() {
+    window.scrollTo(0, 1850)
+}
+
+function scroll_contato() {
+    window.scrollTo(0, 2400)
+}
+
+function alert_login() {
+    var id = document.getElementById("nome").value;
+    alert("Mensagem enviada com sucesso!!" + id);
+}
